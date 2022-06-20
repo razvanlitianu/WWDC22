d0d7dc44af4e2cb5f24f28c3d4081eba9376045b
Razvan Litianu <litianu.razvan@gmail.com>
2022-06-20 11:10:51 +0300

    Initial Commit

---
+++ WWDC22/WWDC22App.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC22App.swift
+//  WWDC22
+//
+//  Created by razvan.litianu on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WWDC22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
